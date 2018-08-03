12e0311316a5e5d7be3c3121e148110152e06450
lgapontes <lgapontes@gmail.com>
2018-08-02 23:27:11 -0300

    Arquivo de apoio para salvar arquivos no celular

--- ArquivoHelper.java
+++ ArquivoHelper.java
@@ -9,33 +9,26 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class ArquivoHelper {
-
     public static String[] listarArquivos(Context context) {
         File diretorio = context.getFilesDir();
         return diretorio.list();
     }
-
     public static void salvarArquivo(Context context, String nome, String conteudo) throws IOException {
         FileOutputStream fileOutputStream = context.openFileOutput(nome, Context.MODE_PRIVATE);
         fileOutputStream.write(conteudo.getBytes());
         fileOutputStream.close();
     }
-
     public static String lerArquivo(Context context, String nome) throws IOException {
         FileInputStream fileInputStream = context.openFileInput(nome);
         BufferedReader reader = new BufferedReader(new InputStreamReader(fileInputStream));
-
         StringBuilder conteudo = new StringBuilder();
         String linha;
-
         while ( (linha = reader.readLine()) != null) {
             conteudo.append(linha + "\n");
         }
-
         fileInputStream.close();
         return conteudo.toString();
     }
-
     public static void apagarArquivo(Context context, String nome) {
         String path = context.getFilesDir() + "/" + nome;
         File arquivo = new File(path);
@@ -43,6 +36,5 @@ public class ArquivoHelper {
             arquivo.delete();
         }
     }
-
 }
 
